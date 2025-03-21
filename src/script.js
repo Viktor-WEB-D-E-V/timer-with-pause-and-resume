0d04b42ae007297e8c881670ff38c4d07a58ef4a
Viktor Kalyn <viktorkalynwebdev@gmail.com>
2025-03-20 20:58:57 -0900

    Initialize Timer class and display default time format

--- src/script.js
+++ src/script.js
@@ -4,3 +4,35 @@ const refs = {
   stopBtn: document.querySelector('.timer-btn[data-stop]'),
 };
 
+class Timer {
+  constructor(time) {
+    this.time = time;
+
+    this.init();
+  }
+
+  init() {
+
+    const { hours, mins, secs } = this.getTimeComponents(0);
+    const markup = `Hours:${hours} Minutes:${mins} Seconds:${secs}`;
+    
+    refs.clockface.insertAdjacentHTML('afterbegin', markup);
+  }
+
+  getTimeComponents(time) {
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    );
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+    return { hours, mins, secs };
+  }
+
+  pad(value) {
+    return String(value).padStart(2, '0');
+  }
+}
+
+
+const timer = new Timer(Date.now())
