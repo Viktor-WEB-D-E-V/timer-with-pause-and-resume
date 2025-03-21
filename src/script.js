1455abdace682387d0d84ffc0d9b33d38e16c517
Viktor Kalyn <viktorkalynwebdev@gmail.com>
2025-03-20 22:13:56 -0900

    Feature: Added start and stop functionality

--- src/script.js
+++ src/script.js
@@ -5,21 +5,44 @@ const refs = {
 };
 
 class Timer {
-  constructor(time) {
-    this.time = time;
+  constructor({ onTick }) {
+    this.timerId = null;
+    this.active = false;
 
+    this.onTick = onTick;
     this.init();
   }
-
+  //The method displays a default time
   init() {
-
-    const { hours, mins, secs } = this.getTimeComponents(0);
-    const markup = `Hours:${hours} Minutes:${mins} Seconds:${secs}`;
-    
-    refs.clockface.insertAdjacentHTML('afterbegin', markup);
+    this.onTick(this.getTimeComponents());
   }
 
-  getTimeComponents(time) {
+  start() {
+    //Prevent multiple timer starts
+    if (this.active) return;
+
+    const startTime = Date.now();
+    //Change the state to active
+    this.active = true;
+    //Refresh markup every 1 sec
+    this.timerId = setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = currentTime - startTime;
+      const time = this.getTimeComponents(deltaTime);
+
+      this.onTick(time);
+    }, 1000);
+  }
+
+  stop() {
+    clearInterval(this.timerId);
+    this.active = false;
+    const time = this.getTimeComponents();
+    this.onTick(time);
+  }
+
+  // The methods converts time(milliseconds) to other values, such as hours, minutes and seconds
+  getTimeComponents(time = 0) {
     const hours = this.pad(
       Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
     );
@@ -28,11 +51,25 @@ class Timer {
 
     return { hours, mins, secs };
   }
-
+  //The methods adds 0 to the number if the string has < 2 characters
   pad(value) {
     return String(value).padStart(2, '0');
   }
 }
 
+const timer = new Timer({
+  //
+  onTick: updateTimerInterface,
+});
 
-const timer = new Timer(Date.now())
+refs.startBtn.addEventListener('click', () => {
+  timer.start();
+});
+
+refs.stopBtn.addEventListener('click', () => {
+  timer.stop();
+});
+
+function updateTimerInterface({ hours, mins, secs }) {
+  refs.clockface.textContent = `Hours: ${hours} Minutes: ${mins} Seconds: ${secs}`;
+}
