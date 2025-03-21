02ce2f6d9ff6403530caf7010084a9a5289ec24b
Viktor Kalyn <viktorkalynwebdev@gmail.com>
2025-03-20 20:47:42 -0900

    Added refernces for clockface and btns

---
+++ src/script.js
@@ -0,0 +1,6 @@
+const refs = {
+  clockface: document.querySelector('.clockface'),
+  startBtn: document.querySelector('.timer-btn[data-start]'),
+  stopBtn: document.querySelector('.timer-btn[data-stop]'),
+};
+
